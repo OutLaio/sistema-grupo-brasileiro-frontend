52f5d8ab0c73e691e90d3e7ffcbba3bdeac5200a
Dan Souza <darleyble3@gmail.com>
2024-08-11 18:49:23 -0300

    routes added

--- sistema-grupo-brasileiro/src/app/app-routing.module.ts
+++ sistema-grupo-brasileiro/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckRequestsComponent } from './modules/feature/check-requests/check-requests.component';
+import { CollaboratorSystemComponent } from './modules/feature/collaborator-system/collaborator-system.component';
+import { MainComponent } from './modules/core/profile/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'acompanhamento', component: CheckRequestsComponent },
+  { path: 'colaboradores', component: CollaboratorSystemComponent },
+  { path: 'perfil', component: MainComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
