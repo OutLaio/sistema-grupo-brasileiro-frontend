bb9246937d0a2cf42d8257c6f4739f743081351a
Dan Souza <darleyble3@gmail.com>
2024-09-30 08:47:19 -0300

    updates

--- sistema-grupo-brasileiro/src/app/modules/services/login-register/login-register.service.ts
+++ sistema-grupo-brasileiro/src/app/modules/services/login-register/login-register.service.ts
@@ -8,30 +8,42 @@ import { LoginResponse } from '../../types/login-response.type';
   providedIn: 'root',
 })
 export class LoginRegisterService {
-  constructor(private httpClient: HttpClient) {}
+  private readonly prefix = 'http://localhost:8080/api/v1/auth'; // Variável para o prefixo da porta
+
+  constructor(private httpClient: HttpClient) { }
 
   registerUser(
     name: string,
     lastname: string,
     email: string,
     password: string,
-    phonenumber: string,
+    phoneNumber: string,
     sector: string,
     occupation: string,
-    nop: string
+    agency: string,
+    avatar: number = 0,
+    profile: number = 0
   ) {
-    return this.httpClient
-      .post('http://localhost:8080/auth/register', {
+    const payload = {
+      employeeForm: {
         name,
         lastname,
-        email,
-        password,
-        phonenumber,
+        phoneNumber,
         sector,
         occupation,
-        nop,
-      });
+        agency,
+        avatar,
+      },
+      userForm: {
+        email,
+        password,
+        profile,
+      }
+    };
+
+    return this.httpClient.post(`${this.prefix}/register`, payload);
   }
+
 
   registerCollaborator(
     name: string,
@@ -43,9 +55,10 @@ export class LoginRegisterService {
     occupation: string,
     nop: string,
     role: string,
+    avatar: number = 1
   ) {
     return this.httpClient
-      .post('http://localhost:8080/auth/register', {
+      .post(`${this.prefix}/register`, {
         name,
         lastname,
         email,
@@ -55,12 +68,13 @@ export class LoginRegisterService {
         occupation,
         nop,
         role,
+        avatar
       });
   }
 
   loginUser(email: string, password: string) {
     return this.httpClient
-      .post<LoginResponse>('http://localhost:8080/auth/login', {
+      .post<LoginResponse>(`${this.prefix}/login`, {
         email,
         password,
       })
@@ -72,13 +86,13 @@ export class LoginRegisterService {
       );
   }
 
-  recoveryPassword(email: string){
+  recoveryPassword(email: string) {
     return this.httpClient
-     .post('http://localhost:8080/recoveryPassword', { email });
+      .post(`${this.prefix}/requestReset`, { email });
   }
 
-  resetPassword(newPassword:string, token:string){
+  resetPassword(newPassword: string, token: string) {
     return this.httpClient
-     .post('http://localhost:8080/resetPassword', { newPassword, token });
+      .post(`${this.prefix}/resetPassword`, { newPassword, token });
   }
 }
