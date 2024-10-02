faa72f4e6b49a0b2bdaec81ea7be744ee307bb10
Dan Souza <darleyble3@gmail.com>
2024-10-02 12:46:23 -0300

    refact

--- sistema-grupo-brasileiro/src/app/modules/feature/collaborator/list-collaborators/component/list-collaborators.component.ts
+++ sistema-grupo-brasileiro/src/app/modules/feature/collaborator/list-collaborators/component/list-collaborators.component.ts
@@ -1,43 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { CollaboratorSystemService } from '../../services/collaborator-system.service'; 
-// @Component({
-//   selector: 'app-list-collaborators',
-//   templateUrl: './list-collaborators.component.html',
-//   styleUrls: ['./list-collaborators.component.css']
-// })
-// export class ListCollaboratorsComponent implements OnInit {
-//   colaboradores: any[] = [];
-//   p: number = 1;
-//   itemsPerPage: number = 5;
-//   selectedColaborador: any = null;
-
-//   constructor(private collaboratorService: CollaboratorSystemService) {}
-
-//   ngOnInit() {
-//     this.loadCollaborators();
-//   }
-
-//   loadCollaborators() {
-//     this.collaboratorService.getCollaborators(this.p - 1, this.itemsPerPage)
-//       .subscribe(data => {
-//         this.colaboradores = data;
-//       });
-//   }
-
-//   selectColaborador(colaborador: any) {
-//     this.selectedColaborador = this.selectedColaborador === colaborador ? null : colaborador;
-//   }
-
-//   isSelected(colaborador: any): boolean {
-//     return this.selectedColaborador === colaborador;
-//   }
-
-//   onPageChange(page: number) {
-//     this.p = page;
-//     this.loadCollaborators();
-//   }
-// }
-
 import { Component } from '@angular/core';
 import { faker } from '@faker-js/faker';
 @Component({
@@ -48,7 +8,7 @@ import { faker } from '@faker-js/faker';
 export class ListCollaboratorsComponent {
   colaboradores: { nome: string; numero: string; email: string; funcao: string; setor: string; agencia: string; }[] = [];
   p: number = 1;
-  itemsPerPage: number = 5;
+  itemsPerPage: number = 8;
 
   constructor() {
     this.generateCollaborators();
