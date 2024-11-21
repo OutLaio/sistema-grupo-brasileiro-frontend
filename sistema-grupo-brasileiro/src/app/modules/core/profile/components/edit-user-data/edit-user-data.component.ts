2f2b186f005588f157aee6b63d3081ad4bb62fbb
Dan Souza <darleyble3@gmail.com>
2024-11-21 01:26:35 -0300

    update

--- sistema-grupo-brasileiro/src/app/modules/core/profile/components/edit-user-data/edit-user-data.component.ts
+++ sistema-grupo-brasileiro/src/app/modules/core/profile/components/edit-user-data/edit-user-data.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { I_Employee_View_Data } from '../../../../shared/interfaces/user/view/employee-view';
+import { I_Employee_Form_Data } from '../../../../shared/interfaces/user/form/employee-form';
 import { ProfileService } from '../../services/profile/profile.service';
 import { Router } from '@angular/router';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
@@ -14,6 +15,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 export class EditUserDataComponent {
   editForm!: FormGroup;
   userProfile!: I_Employee_View_Data | null;
+  userProfileEdit!: I_Employee_Form_Data | null;
 
   constructor(
     private profileService: ProfileService,
@@ -29,6 +31,17 @@ export class EditUserDataComponent {
 
   ngOnInit(): void {
     this.userProfile = this.profileService.getUserProfile();
+    if (this.userProfile) {
+      this.userProfileEdit = {
+        name: this.userProfile.name || '',
+        lastname: this.userProfile.lastname || '',
+        phoneNumber: this.userProfile.phoneNumber || '',
+        sector: this.userProfile.sector || '',
+        occupation: this.userProfile.occupation || '',
+        agency: this.userProfile.agency || '',
+        avatar: this.userProfile.avatar,
+      };
+    }
 
     this.editForm = new FormGroup({
       name: new FormControl(this.userProfile?.name, [Validators.required]),
@@ -44,44 +57,52 @@ export class EditUserDataComponent {
     });
   }
 
-  get name() {
-    return this.editForm.get('name')!;
-  }
-  get lastname() {
-    return this.editForm.get('lastname')!;
-  }
-  get phone() {
-    return this.editForm.get('phone')!;
-  }
-  get sector() {
-    return this.editForm.get('sector')!;
-  }
-  get occupation() {
-    return this.editForm.get('occupation')!;
-  }
-  get nop() {
-    return this.editForm.get('nop')!;
-  }
+  get name() { return this.editForm.get('name')!; }
+  get lastname() { return this.editForm.get('lastname')!; }
+  get phone() { return this.editForm.get('phone')!; }
+  get sector() { return this.editForm.get('sector')!; }
+  get occupation() { return this.editForm.get('occupation')!; }
+  get nop() { return this.editForm.get('nop')!; }
 
   submit() {
     if (this.editForm.invalid) {
-      this.toastrService.error("Preencha todos os campos!")
+      this.toastrService.error("Preencha todos os campos!");
       return;
     }
-    this.userProfile!.name = this.name.value;
-    this.userProfile!.lastname = this.lastname.value;
-    this.userProfile!.phoneNumber = this.phone.value;
-    this.userProfile!.sector = this.sector.value;
-    this.userProfile!.occupation = this.occupation.value;
-    this.userProfile!.agency = this.nop.value;
-    this.profileService.updateProfileUser(this.userProfile!).subscribe((response) => {
-      console.log(response);
-      this.toastrService.success("Dados atualizados com sucesso!");
-      this.dialogRef.close(this.userProfile); 
-    });
+
+    this.userProfileEdit!.name = this.name.value;
+    this.userProfileEdit!.lastname = this.lastname.value;
+    this.userProfileEdit!.phoneNumber = this.phone.value;
+    this.userProfileEdit!.sector = this.sector.value;
+    this.userProfileEdit!.occupation = this.occupation.value;
+    this.userProfileEdit!.agency = this.nop.value;
+
+    this.profileService.updateProfileUser(this.userProfileEdit!, this.userProfile?.id).subscribe(
+      (response) => {
+        this.toastrService.success("Dados atualizados com sucesso!");
+
+        this.profileService.getUserProfileFromServer(this.userProfile?.id).subscribe(
+          (updatedProfile) => {
+            sessionStorage.setItem('userProfile', JSON.stringify(updatedProfile));
+            setTimeout(() => {
+              location.reload();
+            }, 1000);
+          },
+          (error) => {
+            this.toastrService.error("Erro ao atualizar o perfil, por favor tente novamente.");
+          }
+        );
+
+        this.dialogRef.close(this.userProfile);
+      },
+      (error) => {
+        this.toastrService.error("Erro ao atualizar os dados, por favor tente novamente.");
+      }
+    );
   }
 
+
   cancel() {
-    this.dialogRef.close(this.userProfile); 
+    this.dialogRef.close(this.userProfile);
   }
 }
