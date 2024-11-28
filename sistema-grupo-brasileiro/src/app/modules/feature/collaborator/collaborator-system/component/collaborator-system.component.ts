c17501cdbb8123a1e695bc0bca3c839037afe7b8
Dan Souza <darleyble3@gmail.com>
2024-11-28 15:10:53 -0300

    updates

--- sistema-grupo-brasileiro/src/app/modules/feature/collaborator/collaborator-system/component/collaborator-system.component.ts
+++ sistema-grupo-brasileiro/src/app/modules/feature/collaborator/collaborator-system/component/collaborator-system.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { LoginRegisterService } from '../../../../services/login-register/login-register.service';
 import { Router } from '@angular/router';
 import { StorageService } from '../../../../services/storage/storage.service';
@@ -9,18 +9,29 @@ import { StorageService } from '../../../../services/storage/storage.service';
 	styleUrl: './collaborator-system.component.css'
 })
 export class CollaboratorSystemComponent {
-
 	constructor(
-    private storageService: StorageService,
-    private router: Router){}
+		private storageService: StorageService,
+		private router: Router
+	) {}
+
+	isFormVisible: boolean = true;
+	isMobile: boolean = false;
 
 	ngOnInit() {
 		if (this.storageService.getUserRole() !== 'ROLE_SUPERVISOR') {
 			this.router.navigate(['/acompanhamento']);
 		}
+		this.checkIfMobile(); // Verificar se é mobile ao iniciar
 	}
 
-	isFormVisible: boolean = true;
+	@HostListener('window:resize', [])
+	onResize() {
+		this.checkIfMobile();
+	}
+
+	private checkIfMobile() {
+		this.isMobile = window.innerWidth <= 768; // Define 768px como ponto de corte para mobile
+	}
 
 	showForm() {
 		this.isFormVisible = true;
